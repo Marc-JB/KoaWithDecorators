de4ce9e640a057e65efb26d6a403f449ccbaae6f
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-12-23 18:29:08 +0000

    Bugfixes

--- src/main/preview/openApi.ts
+++ src/main/preview/openApi.ts
@@ -1,4 +1,5 @@
 import { getEndpoint } from "../core/getEndpoint"
+import { HttpStatusCodes } from "../HttpStatusCodes"
 import { IndexableParameter, InferConstructorType, Parameter } from "../types"
 
 function removeSurroundings(str: string, char: string): string {
@@ -24,27 +25,30 @@ export function getOpenApiPathsJson<
             .map(it => it.startsWith(":") ? `{${it.substr(1)}}` : it)
         const path = [...basePath, ...routePath]
         const url = "/" + path.join("/")
-        paths[url] = paths[url] ?? {}
+        if (!(url in paths)) {
+            paths[url] = {}
+        }
 
         const spec: {[key: string]: any} = {
             summary: `${instance.constructor.name}.${id}`,
-            operationId: id,
-            consumes: [ "application/json" ],
-            produces: [ "application/json" ]
+            operationId: `${instance.constructor.name}.${id}`,
+            responses: {}
+        }
+
+        for (const desc in HttpStatusCodes) {
+            const code = (HttpStatusCodes as {[key: string]: number})[desc]
+            spec.responses[code.toString()] = { description: desc }
         }
 
         const params = route.params ?? new Map<number, Parameter | IndexableParameter>()
 
         for (const [_, param] of params) {
             if (param.type === "body") {
-                spec.parameters = spec.parameters ?? []
-                spec.parameters.add({
-                    in: "body",
-                    name: "body",
-                    schema: {
-                        type: "object"
+                spec.requestBody = {
+                    content: {
+                        "application/json": {}
                     }
-                })
+                }
             } else if (param.type === "param") {
                 spec.parameters = spec.parameters ?? []
                 spec.parameters.add({
@@ -58,7 +62,6 @@ export function getOpenApiPathsJson<
                 spec.parameters.add({
                     in: "header",
                     name: param.name,
-                    required: true,
                     type: "string"
                 })
             } else if (param.type === "query") {
@@ -81,12 +84,13 @@ export function getOpenApiPathsJson<
         }
 
         const method = route.method ?? "*"
-        paths[url][method] = {
-            operationId: `${instance.constructor.name} - ${id}`
+        for (const mtd of method === "*" ? ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"] : [method]) {
+            paths[url][mtd.toLowerCase()] = spec
         }
     }
 
     return { 
+        openapi: "3.0.1",
         paths, 
         components: { 
             securitySchemes: {
